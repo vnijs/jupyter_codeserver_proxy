923da8f34c13cae7da62fa84ec5a35cac9e8f793
vnijs <radiant@rady.ucsd.edu>
2019-04-15 15:07:38 -0700

    working dir

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Code Server
 http://coder.com is a port of microsoft VScode to the browser
 
 This package is a plugin for `jupyter-server-proxy` <https://jupyter-server-proxy.readthedocs.io/>
-that lets you run an instance of code-server alongside your notebook, for example, 
+that lets you run an instance of code-server alongside your notebook, for example,
 in a JupyterHub / Binder environment.
 
 Installing code-server
@@ -13,8 +13,7 @@ Installing code-server
 RUN	cd /opt && \
 	mkdir /opt/code-server && \
 	cd /opt/code-server && \
-	wget -qO- https://github.com/codercom/code-server/releases/download/1.32.0-245/code-server-1.32.0-245-linux-x64.tar.gz | tar zxvf - --strip-components=1
+	wget -qO- https://github.com/codercom/code-server/releases/download/1.604-vsc1.32.0/code-server-1.604-vsc1.32.0-linux-x64.tar.gz | tar zxvf - --strip-components=1
 
 ENV	PATH=/opt/code-server:$PATH
 ```
-
--- jupyter_codeserver_proxy/__init__.py
+++ jupyter_codeserver_proxy/__init__.py
@@ -7,31 +7,43 @@ for more information.
 import os
 import shutil
 
+
 def setup_codeserver():
     # Make sure codeserver is in $PATH
     def _codeserver_command(port):
-        full_path = shutil.which('code-server')
+        full_path = shutil.which("code-server")
         if not full_path:
-            raise FileNotFoundError('Can not find code-server in $PATH')
+            raise FileNotFoundError("Can not find code-server in $PATH")
         working_dir = os.getenv("CODE_WORKINGDIR", None)
+        if os.path.isdir("/Users/vnijs/git") is False:
+            working_dir = None
         if working_dir is None:
             working_dir = os.getenv("JUPYTER_SERVER_ROOT", ".")
         data_dir = os.getenv("CODE_USER_DATA_DIR", "")
-        if data_dir is not "":
+        if data_dir != "":
             data_dir = "--user-data-dir=" + str(data_dir)
         extensions_dir = os.getenv("CODE_EXTENSIONS_DIR", "")
-        if extensions_dir is not "":
+        if extensions_dir != "":
             extensions_dir = "--extensions-dir=" + str(extensions_dir)
 
-        return [full_path, '--port=' + str(port), "--allow-http", "--no-auth", "--vanilla", data_dir, extensions_dir, working_dir]
+        return [
+            full_path,
+            "--port=" + str(port),
+            "--allow-http",
+            "--no-auth",
+            "--vanilla",
+            data_dir,
+            extensions_dir,
+            working_dir,
+        ]
 
     return {
-        'command': _codeserver_command,
-        'timeout': 20,
-        'launcher_entry': {
-            'title': 'VS Code IDE',
-            'icon_path': os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                      'icons', 'vscode.svg')
-        }
+        "command": _codeserver_command,
+        "timeout": 20,
+        "launcher_entry": {
+            "title": "VS Code IDE",
+            "icon_path": os.path.join(
+                os.path.dirname(os.path.abspath(__file__)), "icons", "vscode.svg"
+            ),
+        },
     }
-
